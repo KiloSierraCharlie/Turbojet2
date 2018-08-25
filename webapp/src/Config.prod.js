6be8e14556a598445d592ebb09ab1d21ddd5cbd1
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2018-08-25 23:33:10 +0200

    Corrected typo in previous commit

--- webapp/src/Config.prod.js
+++ webapp/src/Config.prod.js
@@ -3,7 +3,7 @@
 * Remember to set the correct development endpoint and apikey
 */
 export default {
-    endpoint: 'https//api.fteturbojet.com/',
+    endpoint: 'https://api.fteturbojet.com/',
     apikey: 'rpF88UUHiheBBYiC1KCbTsZDkuP1kQvq',
     recaptchaKey: '6LeS8k8UAAAAAOZsYkWOV9dSei8RNa2sSDFNI6hB',
     lang: 'en',
