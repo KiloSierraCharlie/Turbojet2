488dea0b88570d8ab5a6a0da8544db067a06e5d1
kevin bouhadana <kevin.bouhadana@gmail.com>
2018-08-25 20:18:38 +0200

    added dynamic version in the footer from config file

--- webapp/src/Config.dev.js
+++ webapp/src/Config.dev.js
@@ -6,5 +6,6 @@ export default {
     endpoint: 'http://api.turbojet.local/',
     apikey: 'rpF88UUHiheBBYiC1KCbTsZDkuP1kQvq',
     recaptchaKey: '6LeS8k8UAAAAAOZsYkWOV9dSei8RNa2sSDFNI6hB',
-    lang: 'en'
+    lang: 'en',
+    version: '2.0.2'
 }
--- webapp/src/Config.prod.js
+++ webapp/src/Config.prod.js
@@ -6,5 +6,6 @@ export default {
     endpoint: 'http://api.fteturbojet.com/',
     apikey: 'rpF88UUHiheBBYiC1KCbTsZDkuP1kQvq',
     recaptchaKey: '6LeS8k8UAAAAAOZsYkWOV9dSei8RNa2sSDFNI6hB',
-    lang: 'en'
+    lang: 'en',
+    version: '2.0.2'
 }
