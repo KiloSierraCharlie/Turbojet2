f0be2d585fc5d912e2fc2c8ad898a3ecbe4e3d13
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2019-04-01 13:42:30 +0200

    Revert back to normal filename for build.js

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist/',
-        filename: 'build.[hash].js'
+        filename: 'build.js'
     },
     resolve: {
         extensions: ['*', '.js', '.vue', '.json'],
